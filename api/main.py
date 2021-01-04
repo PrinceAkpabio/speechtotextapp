ba575d9b333a4a39b9acdc22e85d4051a3d2b4dc
prince <princeakpabio90@gmail.com>
2021-01-04 03:16:03 +0100

    changed app.run totally

--- api/main.py
+++ api/main.py
@@ -49,4 +49,4 @@ def SpeechReg():
             }
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+    app.run(debug=app.config['DEBUG'], port=app.config['PORT'])
