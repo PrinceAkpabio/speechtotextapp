08ea6945bde13ac7e20f75a7ca3ae3978cf82a1c
prince <princeakpabio90@gmail.com>
2021-01-04 18:09:27 +0100

    changed build folder path in main.py

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import speech_recognition as sr
 
 # app = Flask(__name__)
-app = Flask(__name__, static_folder='./build', static_url_path='/')
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 # @app.route('/')
 # def index():
